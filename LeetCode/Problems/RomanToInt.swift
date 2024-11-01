765431b8cdc956e24742f1452fd74dab0ad39836
Nithya Vasudevan <nithya1vasudevan@gmail.com>
2024-11-01 12:28:31 +0100

    Remove Warnings

--- LeetCode/Problems/RomanToInt.swift
+++ LeetCode/Problems/RomanToInt.swift
@@ -5,7 +5,7 @@ class RomanToInt {
         let dict : [Character : Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
         var sum = 0, prev = 0
         for char in s {
-            var cur = dict[char] ?? 0
+            let cur = dict[char] ?? 0
             sum += (prev >= cur) ? prev: -prev
             prev = cur
         }
